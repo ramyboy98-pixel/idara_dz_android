4b084a8c6b94be368916552147463ffd80ba3fe0
ramyboy98-pixel <ramyboy98@gmail.com>
2026-05-25 17:38:45 +0100

    Add files via upload

--- lib/core/paths/app_paths.dart
+++ lib/core/paths/app_paths.dart
@@ -1,1 +1,8 @@
+import 'package:path_provider/path_provider.dart';
 
+class AppPaths {
+  static Future<String> appDocumentsPath() async {
+    final dir = await getApplicationDocumentsDirectory();
+    return dir.path;
+  }
+}
