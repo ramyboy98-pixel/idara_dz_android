d23c82d320a29b9a06bada6c5ef176fd3ba77f0f
ramyboy98-pixel <ramyboy98@gmail.com>
2026-05-26 12:55:12 +0100

    Update custom_document_template.dart

--- lib/data/models/custom_document_template.dart
+++ lib/data/models/custom_document_template.dart
@@ -5,16 +5,6 @@ class CustomDocumentTemplate {
     required this.title,
     this.description,
     this.templateFilePath,
-    this.editorContent,
-    this.pageSize = 'A4',
-    this.orientation = 'portrait',
-    this.marginTop = 32,
-    this.marginRight = 32,
-    this.marginBottom = 32,
-    this.marginLeft = 32,
-    this.textDirection = 'rtl',
-    this.baseFontSize = 14,
-    this.lineSpacing = 6,
     required this.createdAt,
     this.fieldsCount = 0,
   });
@@ -24,16 +14,6 @@ class CustomDocumentTemplate {
   final String title;
   final String? description;
   final String? templateFilePath;
-  final String? editorContent;
-  final String pageSize;
-  final String orientation;
-  final double marginTop;
-  final double marginRight;
-  final double marginBottom;
-  final double marginLeft;
-  final String textDirection;
-  final double baseFontSize;
-  final double lineSpacing;
   final DateTime createdAt;
   final int fieldsCount;
 
@@ -44,16 +24,6 @@ class CustomDocumentTemplate {
       title: map['title'] as String,
       description: map['description'] as String?,
       templateFilePath: map['template_file_path'] as String?,
-      editorContent: map['editor_content'] as String?,
-      pageSize: (map['page_size'] as String?) ?? 'A4',
-      orientation: (map['orientation'] as String?) ?? 'portrait',
-      marginTop: _toDouble(map['margin_top'], 32),
-      marginRight: _toDouble(map['margin_right'], 32),
-      marginBottom: _toDouble(map['margin_bottom'], 32),
-      marginLeft: _toDouble(map['margin_left'], 32),
-      textDirection: (map['text_direction'] as String?) ?? 'rtl',
-      baseFontSize: _toDouble(map['base_font_size'], 14),
-      lineSpacing: _toDouble(map['line_spacing'], 6),
       createdAt: DateTime.tryParse(map['created_at'] as String? ?? '') ?? DateTime.now(),
       fieldsCount: (map['fields_count'] as int?) ?? 0,
     );
@@ -66,23 +36,7 @@ class CustomDocumentTemplate {
       'title': title,
       'description': description,
       'template_file_path': templateFilePath,
-      'editor_content': editorContent,
-      'page_size': pageSize,
-      'orientation': orientation,
-      'margin_top': marginTop,
-      'margin_right': marginRight,
-      'margin_bottom': marginBottom,
-      'margin_left': marginLeft,
-      'text_direction': textDirection,
-      'base_font_size': baseFontSize,
-      'line_spacing': lineSpacing,
       'created_at': createdAt.toIso8601String(),
     };
-  }
-
-  static double _toDouble(Object? value, double fallback) {
-    if (value is num) return value.toDouble();
-    if (value is String) return double.tryParse(value) ?? fallback;
-    return fallback;
   }
 }
